9d0457be52dafef32a27a73c06ecef8814b88568
Ayana Mello <ayanamello27@gmail.com>
2021-09-09 12:44:37 -0300

    feat: create input component

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,6 +3,7 @@ import { extendTheme, theme as ChakraTheme } from "@chakra-ui/react";
 export const theme = extendTheme({
     colors: {
         green: {
+            50: "#E3F2EF",
             300: "#73C0B0",
             400: "#488788"
         },
@@ -15,35 +16,40 @@ export const theme = extendTheme({
         },
         gray: {
             50: "#F9F9F9",
+            60: "#F3F2F2",
             100: "#DDDDDD",
+            200: "#8C8C8C",
             400: "#6A6A6A",
             800: "#373737"
         },
-        fonts: {
-            heading: "Didact Gothic",
-            body: "Didact Gothic",
-        },
-        fontSizes: {
-            xs: '0.75rem',
-            sm: '0.875rem',
-            md: '1rem',
-            lg: '1.125rem',
-            xl: '1.25rem',
-            '2xl': '1.5rem',
-            '3xl': '1.875rem',
-            '4xl': '2.25rem',
-            '5xl': '3rem',
-            '6xl': '3.75rem',
-            '7xl': '4.5rem',
-            '8xl': '6rem',
-            '9xl': '8rem',
-        },
-        styles: {
-            global: {
-                body: {
-                    bg: "gray.50",
-                    color: "black"
-                },
+        red: {
+            500: "#FF2E00"
+        }
+    },
+    fonts: {
+        heading: "Didact Gothic",
+        body: "Didact Gothic",
+    },
+    fontSizes: {
+        xs: '0.75rem',
+        sm: '0.875rem',
+        md: '1rem',
+        lg: '1.125rem',
+        xl: '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '1.875rem',
+        '4xl': '2.25rem',
+        '5xl': '3rem',
+        '6xl': '3.75rem',
+        '7xl': '4.5rem',
+        '8xl': '6rem',
+        '9xl': '8rem',
+    },
+    styles: {
+        global: {
+            body: {
+                bg: "gray.50",
+                color: "black"
             },
         },
     },
