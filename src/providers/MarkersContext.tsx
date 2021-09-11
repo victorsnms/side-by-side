7666ca484af8cb99c7da375490f715f5e3e6aedc
Victor Martins <victorsantows@gmail.com>
2021-09-11 17:35:54 -0300

    feat(providers): added EventsContext, MarkersContext and WastecolContext

---
+++ src/providers/MarkersContext.tsx
@@ -0,0 +1,67 @@
+//necessita passar para o app provider
+import { AxiosResponse } from "axios";
+import {
+  createContext,
+  useContext,
+  useState,
+  ReactNode,
+  useCallback,
+} from "react";
+import { api } from "../services/api";
+
+interface MarkersProviderProps {
+  children: ReactNode;
+}
+
+interface Marker {
+  lat: number;
+  lng: number;
+  time: Date;
+  type: string;
+  title: string;
+  description?: string;
+}
+
+interface MarkersContextData {
+  markers: Marker[];
+  createMarker: (data: Marker, accessToken: string) => Promise<void>;
+}
+
+const MarkersContext = createContext<MarkersContextData>(
+  {} as MarkersContextData
+);
+
+const useMarkers = () => {
+  const context = useContext(MarkersContext);
+
+  if (!context) {
+    throw new Error("useMarkers must be used within an MarkersProvider");
+  }
+  return context;
+};
+
+const MarkersProvider = ({ children }: MarkersProviderProps) => {
+  const [markers, setMarkers] = useState<Marker[]>([]);
+
+  const createMarker = useCallback(
+    async (data: Marker, accessToken: string) => {
+      api
+        .post("/markers", data, {
+          headers: { Authorization: `Bearer ${accessToken}` },
+        })
+        .then((response: AxiosResponse<Marker>) =>
+          setMarkers((oldMarkers) => [...oldMarkers, response.data])
+        )
+        .catch((err) => console.log(err));
+    },
+    []
+  );
+
+  return (
+    <MarkersContext.Provider value={{ markers, createMarker }}>
+      {children}
+    </MarkersContext.Provider>
+  );
+};
+
+export { useMarkers, MarkersProvider };
