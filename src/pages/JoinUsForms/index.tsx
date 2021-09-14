fab88da1539207bfbaf0dcd4a1aa53195a76fadb
Ayana Mello <ayanamello27@gmail.com>
2021-09-14 20:06:52 -0300

    fix: add useBreakpointValue hook to joinUs page

--- src/pages/JoinUsForms/index.tsx
+++ src/pages/JoinUsForms/index.tsx
@@ -1,21 +1,27 @@
 import { ToggleSwitch } from "../../components/ToggleSwitch";
 import CoolImage from "../../assets/images/image-joinpage.png";
-import { Flex, Center, Box, Image, Text, Link } from "@chakra-ui/react";
+import {
+  Flex,
+  Center,
+  Box,
+  Image,
+  Text,
+  Link,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 import { SignupForm } from "./SignupForm";
 import { LoginForm } from "./LoginForm";
 import { useToggleSwitchContext } from "../../providers/ToggleSwitchContext";
 import { useEffect } from "react";
 
 export const JoinUsForms = () => {
-  const {
-    formOption,
-    setOptions,
-    setFormOption,
-  } = useToggleSwitchContext();
+  const display = useBreakpointValue({ base: "none", lg: "block" });
+
+  const { formOption, setOptions, setFormOption } = useToggleSwitchContext();
 
   useEffect(() => {
     setOptions(["Login", "Sign up"]);
-    if(formOption === undefined) {
+    if (formOption === undefined) {
       setFormOption("Login");
     }
   }, []);
@@ -28,50 +34,39 @@ export const JoinUsForms = () => {
       h={{ lg: "100vh" }}
     >
       <Center>
-        <Box pr={["0", "0", "0", "3em", "8em"]}>
-          {window.innerWidth > 991 ? (
-            <>
-              <Box mb="50px">
-                <Text bgColor="green.300" display="inline">
-                  LOGO
-                </Text>
-                <Box w="280px">
-                  <Text fontWeight="bold" fontSize="20px">
-                    Search! Create! Recycle! Make the world a cleaner place!
-                  </Text>
-                </Box>
-              </Box>
+        <Box pr={["0", "0", "0", "3em", "8em"]} display={display}>
+          <Box mb="50px">
+            <Text bgColor="green.300" display="inline">
+              LOGO
+            </Text>
+            <Box w="280px">
+              <Text fontWeight="bold" fontSize="20px">
+                Search! Create! Recycle! Make the world a cleaner place!
+              </Text>
+            </Box>
+          </Box>
 
-              <Image
-                src={CoolImage}
-                alt="Illustration form page"
-                w={["200px", "240px", "240", "490px", "580px"]}
-                opacity="85%"
-              />
-            </>
-          ) : (
-            <Image
-              src={CoolImage}
-              alt="Illustration form page"
-              w="220px"
-              mt="4vh"
-              mb="5vh"
-              opacity="85%"
-            />
-          )}
+          <Image
+            src={CoolImage}
+            alt="Illustration form page"
+            w={["200px", "240px", "240", "490px", "580px"]}
+            opacity="85%"
+          />
         </Box>
       </Center>
 
       <Center>
-        <Box
+        <Flex
+          direction="column"
+          justify="center"
           bg={{ lg: "white" }}
-          h={{ lg: "460px" }}
+          h={{ base: "100vh", lg: "460px" }}
           w={{ lg: "400px" }}
           shadow={{ lg: "lg" }}
           pt={{ lg: "2.5em" }}
           textAlign="center"
         >
-          <Flex justifyContent="center">
+          <Flex justify="center" mb={{ base: "2em", lg: "0" }}>
             <ToggleSwitch />
           </Flex>
 
@@ -106,7 +101,7 @@ export const JoinUsForms = () => {
               </Text>
             </>
           )}
-        </Box>
+        </Flex>
       </Center>
     </Flex>
   );
