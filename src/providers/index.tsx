57e54add8e949259fbfafbfd121cdca381c3909d
Victor Martins <victorsantows@gmail.com>
2021-09-15 14:33:03 -0300

    feat(component): added LocationContext and map starting on user location

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -6,21 +6,24 @@ import { MarkersProvider } from "./MarkersContext";
 import { ModalProvider } from "./ModalProviders";
 import { UserProvider } from "./UserContext";
 import { ToggleSwitchProvider } from "./ToggleSwitchContext";
+import { LocationProvider } from "./LocationContext";
 
 interface AppProviderProps {
   children: ReactNode;
 }
 
 export const AppProvider = ({ children }: AppProviderProps) => (
-        <ModalProvider>
-  <AuthProvider>
-    <MarkersProvider>
-      <UserProvider>
-        <ToggleSwitchProvider>
-          <ChakraProvider theme={theme}>{children}</ChakraProvider>
-        </ToggleSwitchProvider>
-      </UserProvider>
-    </MarkersProvider>
-  </AuthProvider>
-        </ModalProvider>
+  <ModalProvider>
+    <AuthProvider>
+      <MarkersProvider>
+        <UserProvider>
+          <LocationProvider>
+            <ToggleSwitchProvider>
+              <ChakraProvider theme={theme}>{children}</ChakraProvider>
+            </ToggleSwitchProvider>
+          </LocationProvider>
+        </UserProvider>
+      </MarkersProvider>
+    </AuthProvider>
+  </ModalProvider>
 );
